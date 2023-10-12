21305e1434d3147358c208a8fe593e68aadae19f
vinitus <tlsdnrng@gmail.com>
2023-10-12 17:24:52 +0900

    주석 제거

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,5 @@
 <script>
 	// import Counter from './Counter.svelte';
-	// import welcome from '$lib/images/svelte-welcome.webp';
-	// import welcome_fallback from '$lib/images/svelte-welcome.png';
 	import close from '$lib/images/close.svg';
 	import add from '$lib/images/add.svg';
 </script>
@@ -20,23 +18,7 @@
 		<h1>Reservation</h1>
 		<img class="close" src={close} alt="close" />
 	</header>
-	<!-- <h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" /> 의미 파익하기
-				<img src={welcome_fallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app << 그냥 h1 렌더링
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter /> // 컴포넌트인 듯?-->
-	<!-- <div class="" /> -->
+	<!-- <Counter /> // 컴포넌트인 듯? -->
 </section>
 
 <style>
