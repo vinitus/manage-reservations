a41bd47cdcbdb25550853270d1a0cb1ea0479a85
vinitus <tlsdnrng@gmail.com>
2023-10-12 17:23:42 +0900

    예약 페이지의 헤더 태그 및 css 생성

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,8 @@
 	// import Counter from './Counter.svelte';
 	// import welcome from '$lib/images/svelte-welcome.webp';
 	// import welcome_fallback from '$lib/images/svelte-welcome.png';
+	import close from '$lib/images/close.svg';
+	import add from '$lib/images/add.svg';
 </script>
 
 <svelte:head>
@@ -9,7 +11,15 @@
 	<meta name="description" content="Reservation List" />
 </svelte:head>
 
-<section>
+<section class="wrapper">
+	<header class="header">
+		<button type="button">
+			<img src={add} alt="add" />
+			<p>New Reservation</p>
+		</button>
+		<h1>Reservation</h1>
+		<img class="close" src={close} alt="close" />
+	</header>
 	<!-- <h1>
 		<span class="welcome">
 			<picture>
@@ -26,10 +36,48 @@
 	</h2>
 
 	<Counter /> // 컴포넌트인 듯?-->
-	<div class="wrapper" />
+	<!-- <div class="" /> -->
 </section>
 
 <style>
+	.header {
+		top: 0px;
+		position: absolute;
+		height: 110px;
+		width: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
+
+	.close {
+		width: 32px;
+		height: 32px;
+		margin: 0px 36px 0px 172px;
+	}
+
+	.header button {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		background-color: #fdfdfd;
+		color: #ec551f;
+		width: 220px;
+		height: 60px;
+		border: none;
+		border-radius: 10px;
+		box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
+		margin-left: 20px;
+	}
+
+	.header button p {
+		line-height: 5px;
+		margin-top: 0px;
+		border-bottom-width: 0px;
+		margin-bottom: 0px;
+		margin-left: 10px;
+	}
+
 	.wrapper {
 		width: 977px;
 		height: 793px;
@@ -43,5 +91,6 @@
 		justify-content: center;
 		align-items: center;
 		flex: 0.6;
+		position: fixed;
 	}
 </style>
