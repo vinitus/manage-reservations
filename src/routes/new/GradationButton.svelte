993c7c9145d9d19bc765b0cafb0f5b4991ee6c64
vinitus <tlsdnrng@gmail.com>
2023-10-13 17:34:49 +0900

    button 컴포넌트 모듈화

---
+++ src/routes/new/GradationButton.svelte
@@ -0,0 +1,25 @@
+<script>
+	/**
+	 * @type {string}
+	 */
+	export let imgSrc;
+</script>
+
+<button type="button" class="calendar-open-button">
+	<img src={imgSrc} alt="event_available" />
+	<p>Select Date</p>
+</button>
+
+<style>
+	.calendar-open-button {
+		width: calc((100% - 40px) / 3);
+		height: 60px;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		border: none;
+		border-radius: 10px;
+		background: linear-gradient(to right bottom, #ffffff, #f9f9f8);
+		box-shadow: 0px 1px 2px 1px rgba(0, 0, 0, 0.2);
+	}
+</style>
