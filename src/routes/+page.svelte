9c6e36da6ffcf5d12226c6fe5079513af69577e3
vinitus <tlsdnrng@gmail.com>
2023-10-13 14:35:24 +0900

    Header 컴포넌트 모듈화

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script>
 	// import Counter from './Counter.svelte';
-	import close from '$lib/images/close.svg';
-	import add from '$lib/images/add.svg';
+	import Header from './Header.svelte';
 </script>
 
 <svelte:head>
@@ -10,61 +9,11 @@
 </svelte:head>
 
 <section class="wrapper">
-	<header class="header">
-		<a href="/new">
-			<button type="button">
-				<img src={add} alt="add" />
-				<p>New Reservation</p>
-			</button>
-		</a>
-		<h1>Reservation</h1>
-		<img class="close" src={close} alt="close" />
-	</header>
+	<Header />
 	<!-- <Counter /> // 컴포넌트인 듯? -->
 </section>
 
 <style>
-	.header {
-		top: 0px;
-		position: absolute;
-		height: 110px;
-		width: 100%;
-		display: flex;
-		align-items: center;
-		justify-content: space-between;
-		background-color: #ffffff;
-		border-top-right-radius: 10px;
-		border-top-left-radius: 10px;
-	}
-
-	.close {
-		width: 32px;
-		height: 32px;
-		margin: 0px 36px 0px 172px;
-	}
-
-	.header button {
-		display: flex;
-		align-items: center;
-		justify-content: center;
-		background-color: #fdfdfd;
-		color: #ec551f;
-		width: 220px;
-		height: 60px;
-		border: none;
-		border-radius: 10px;
-		box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
-		margin-left: 20px;
-	}
-
-	.header button p {
-		line-height: 5px;
-		margin-top: 0px;
-		border-bottom-width: 0px;
-		margin-bottom: 0px;
-		margin-left: 10px;
-	}
-
 	.wrapper {
 		width: 977px;
 		height: 793px;
