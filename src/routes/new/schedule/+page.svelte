2c923818b5f80ac3a8bb740c08385433025fc790
vinitus <tlsdnrng@gmail.com>
2023-10-17 15:37:50 +0900

    C:/Program Files/Git/new/schedule 라우팅

---
+++ src/routes/new/schedule/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	export let isOpen: boolean;
+</script>
+
+<section class="" />
+
+<style>
+	section {
+		background-color: #666666;
+		width: 100%;
+		height: 100%;
+		position: absolute;
+		top: 0px;
+		left: 0px;
+		z-index: 100;
+	}
+	.schedule-select-form {
+		position: absolute;
+		width: 400px;
+		height: 420px;
+	}
+</style>
