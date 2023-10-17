25d42b6791d52b5bfcdbc90cb8dcf61f313688ba
vinitus <tlsdnrng@gmail.com>
2023-10-17 17:45:55 +0900

    게스트 숫자 카운터 컴포넌트 모듈화

---
+++ src/routes/new/ReservationGuestsCounter.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import GradationButton from './GradationButton.svelte';
+	import mathMinus from '$lib/images/math-minus.svg';
+	import mathPlus from '$lib/images/math-plus.svg';
+	import type { ReservationInfo } from '$store';
+
+	export let reservationInfo: ReservationInfo;
+</script>
+
+<div class="counter-wrapper">
+	<p>Guests</p>
+	<GradationButton
+		imgSrc={mathMinus}
+		alt="mathMinus"
+		style="width:70px; height:70px;"
+		onClick={() => {
+			reservationInfo.guests -= 1;
+		}}
+	/>
+	<p class="count-num">{reservationInfo.guests}</p>
+	<GradationButton
+		imgSrc={mathPlus}
+		alt="mathPlus"
+		style="width:70px; height:70px;"
+		onClick={() => {
+			reservationInfo.guests += 1;
+		}}
+	/>
+</div>
+
+<style>
+	.count-num {
+		font-size: 24px;
+		font-weight: 600;
+	}
+
+	.counter-wrapper {
+		width: 310px;
+		display: flex;
+		justify-content: space-around;
+		align-items: center;
+	}
+
+	.counter-wrapper * {
+		width: min-content;
+	}
+</style>
