4e4a413afc612de7ef44059afc62bd937d63eff0
vinitus <tlsdnrng@gmail.com>
2023-10-17 20:26:31 +0900

    date, time 상태 연결하기

--- src/routes/new/schedule/+page.svelte
+++ src/routes/new/schedule/+page.svelte
@@ -1,24 +1,54 @@
-<script>
+<script lang="ts">
+	import { newReservation, type MyDate, type Time } from '$store';
 	import GradationButton from '../GradationButton.svelte';
 	import TimeControl from './TimeControl.svelte';
 	import alarmOn from '$lib/images/alarm_on.svg';
 	import today from '$lib/images/today.svg';
 	import trash from '$lib/images/trash.svg';
+
+	let time: Time;
+	let date: MyDate;
+
+	newReservation.subscribe((item) => ({ time, date } = item));
+	const tmp = 12;
 </script>
 
 <section>
 	<div class="schedule-select-form">
 		<div class="row">
 			<img src={alarmOn} alt="alarmOn" />
-			<input value="2:00 PM" />
+			<div class="input-wrapper">
+				<input class="time-input" type="number" value={`${tmp - 12 <= 0 ? tmp : tmp - 12}`} />
+				<p>:</p>
+				<input
+					class="time-input"
+					type="number"
+					value={`${
+						time.minute.toString().length === 1
+							? '0' + time.minute.toString()
+							: time.minute.toString()
+					}`}
+				/>
+				<input
+					type="text"
+					class="time-input"
+					style="width: 30px;"
+					value={`${Number(tmp) - 12 < 0 ? 'AM' : 'PM'}`}
+				/>
+			</div>
 		</div>
 		<div class="row">
 			<img src={today} alt="today" />
-			<input value="May 10" />
+			<div class="input-wrapper">
+				<input type="text" class="month-input" value={date.month} />
+				<input type="number" class="day-input" value={date.day} />
+			</div>
 		</div>
 		<TimeControl />
 		<div class="row">
-			<GradationButton imgSrc={trash} alt="trash" style="width: 68px; height: 52px;" />
+			<GradationButton style="width: 68px; height: 52px;"
+				><a href="/new"> <img src={trash} alt="trash" /></a></GradationButton
+			>
 			<GradationButton
 				style="margin-left:35px; width: 100%; height: 52px; background: none; background-color: #E5501B; color:white; font-size: 18px;"
 				>Save</GradationButton
@@ -57,7 +87,7 @@
 		border-radius: 10px;
 	}
 
-	.row input {
+	.input-wrapper {
 		display: flex;
 		align-items: center;
 		margin-left: 15px;
@@ -68,9 +98,41 @@
 		border-radius: 10px;
 	}
 
-	.row input:focus {
+	.day-input {
+		width: 100%;
+		height: 100%;
+		border: none;
+		outline: none;
+	}
+
+	.month-input {
+		width: 40px;
+		height: 100%;
+		border: none;
+		outline: none;
+	}
+
+	.time-input {
+		width: 22px;
+		height: 100%;
+		border: none;
+		outline: none;
+	}
+
+	.input-wrapper:focus-within {
 		border: 3px solid #fa8960;
 		padding: 0px 27px;
 		outline: none;
+	}
+
+	input[type='number']::-webkit-inner-spin-button,
+	input[type='number']::-webkit-outer-spin-button {
+		-webkit-appearance: none;
+		margin: 0;
+	}
+
+	input[type='number'] {
+		-moz-appearance: textfield;
+		appearance: none;
 	}
 </style>
