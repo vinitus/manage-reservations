a07dc026fdb708a9451fa4852b0737c4b3e14bb7
vinitus <tlsdnrng@gmail.com>
2023-10-18 00:03:04 +0900

    예약 정보를 표시하기 위한 틀잡기

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
-	// import Counter from './Counter.svelte';
 	import Header from './Header.svelte';
+	import ReservationInfo from './ReservationInfo.svelte';
 </script>
 
 <svelte:head>
@@ -10,10 +10,32 @@
 
 <section class="wrapper">
 	<Header isNew={false} />
-	<!-- <Counter /> // 컴포넌트인 듯? -->
+	<div class="reservation-wrapper">
+		<ReservationInfo />
+		<ReservationInfo />
+		<ReservationInfo />
+		<ReservationInfo />
+	</div>
 </section>
 
 <style>
+	.reservation-wrapper {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: space-between;
+		margin-top: 110px;
+		width: 100%;
+		height: 100%;
+		padding: 20px 20px 0 20px;
+		overflow-y: auto;
+		scrollbar-width: none;
+		-ms-overflow-style: none;
+	}
+
+	.reservation-wrapper::-webkit-scrollbar {
+		display: none;
+	}
+
 	.wrapper {
 		width: 977px;
 		height: 793px;
---
+++ src/routes/ReservationInfo.svelte
@@ -0,0 +1,10 @@
+<div class="reservation">HI</div>
+
+<style>
+	.reservation {
+		width: 300px;
+		height: 340px;
+		background-color: #ffffff;
+		margin-bottom: 20px;
+	}
+</style>
