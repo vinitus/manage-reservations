a3b03f079034435111c9471025ffebf1e17eab24
vinitus <tlsdnrng@gmail.com>
2023-10-17 17:06:49 +0900

    newReservation의 초깃값 설정

--- src/store.ts
+++ src/store.ts
@@ -6,19 +6,33 @@ interface MyDate {
 }
 
 interface Time {
+	hour: number;
 	minute: number;
-	second: number;
 }
 
 export interface ReservationInfo {
 	name: string;
 	phone: string;
 	date: MyDate;
-	Time: Time;
+	time: Time;
 	guests: number;
 	table: number[];
 	note: string;
 }
 
 export const reservationList = writable<ReservationInfo[]>([]);
-export const newReservation = writable<ReservationInfo>();
+export const newReservation = writable<ReservationInfo>({
+	name: '',
+	phone: '',
+	date: {
+		month: 1,
+		day: 1
+	},
+	time: {
+		hour: 1,
+		minute: 1
+	},
+	guests: 1,
+	table: [],
+	note: ''
+});
