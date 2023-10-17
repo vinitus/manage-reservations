287a809a621fb9d3145b995cec68fea34d45001b
vinitus <tlsdnrng@gmail.com>
2023-10-18 04:07:18 +0900

    편집페이지

---
+++ src/routes/edit/[slug]/+page.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+	import Header from '../../Header.svelte';
+	import ReservationForm from '../../new/ReservationForm.svelte';
+	import { page } from '$app/stores';
+	import { reservationList, newReservation, dateTimeIsValid, type ReservationInfo } from '$store';
+
+	const idx = $page.url.pathname.split('/')[2];
+
+	let reservationInfo: ReservationInfo;
+
+	reservationList.subscribe((item) => (reservationInfo = item[Number(idx)]));
+
+	newReservation.update(() => reservationInfo);
+
+	dateTimeIsValid.update(() => true);
+</script>
+
+<svelte:head>
+	<title>Edit Reservation</title>
+</svelte:head>
+
+<section>
+	<Header isNew={true} />
+	<ReservationForm
+		submitHandler={() => {
+			reservationList.update((item) => {
+				const jsonStr = JSON.stringify(reservationInfo);
+				const originData = JSON.parse(jsonStr);
+				newReservation.update(() => {
+					return {
+						name: '',
+						phone: '',
+						date: {
+							month: 'May',
+							day: 10
+						},
+						time: {
+							hour: 14,
+							minute: 0
+						},
+						guests: 1,
+						table: [],
+						note: '',
+						isSeated: false
+					};
+				});
+				item[Number(idx)] = originData;
+				return item;
+			});
+		}}
+	/>
+</section>
+
+<style>
+	section {
+		width: 977px;
+		height: 793px;
+		background-color: #f5f5f4;
+		border-radius: 10px;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+		position: fixed;
+	}
+</style>
