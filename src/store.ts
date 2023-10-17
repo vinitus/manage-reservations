5b2adc7f28398fdffffa0497e898cbe0af20f041
vinitus <tlsdnrng@gmail.com>
2023-10-17 21:36:02 +0900

    초기값 수정

--- src/store.ts
+++ src/store.ts
@@ -37,12 +37,12 @@ export const newReservation = writable<ReservationInfo>({
 	name: '',
 	phone: '',
 	date: {
-		month: 'Jan',
-		day: 1
+		month: 'May',
+		day: 10
 	},
 	time: {
-		hour: 1,
-		minute: 1
+		hour: 14,
+		minute: 0
 	},
 	guests: 1,
 	table: [],
