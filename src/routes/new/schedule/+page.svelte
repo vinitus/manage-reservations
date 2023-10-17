f3e4250c321c693210a2368eed88df67bb1cea5c
vinitus <tlsdnrng@gmail.com>
2023-10-17 16:00:35 +0900

    시간, 날짜 input 렌더링

--- src/routes/new/schedule/+page.svelte
+++ src/routes/new/schedule/+page.svelte
@@ -1,22 +1,65 @@
-<script lang="ts">
-	export let isOpen: boolean;
+<script>
+	import alarmOn from '$lib/images/alarm_on.svg';
+	import today from '$lib/images/today.svg';
 </script>
 
-<section class="" />
+<section>
+	<div class="schedule-select-form">
+		<div class="row">
+			<img src={alarmOn} alt="alarmOn" />
+			<input value="2:00 PM" />
+		</div>
+		<div class="row">
+			<img src={today} alt="today" />
+			<input value="May 10" />
+		</div>
+	</div>
+</section>
 
 <style>
-	section {
-		background-color: #666666;
+	.row {
+		display: flex;
+		align-items: center;
 		width: 100%;
-		height: 100%;
-		position: absolute;
-		top: 0px;
-		left: 0px;
-		z-index: 100;
+		margin-bottom: 20px;
 	}
+
+	section {
+		width: 977px;
+		height: 793px;
+		border-radius: 10px;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+		position: fixed;
+	}
+
 	.schedule-select-form {
+		padding: 20px 25px;
 		position: absolute;
 		width: 400px;
 		height: 420px;
+		background-color: #ffffff;
+		border: none;
+		border-radius: 10px;
+	}
+
+	.row input {
+		display: flex;
+		align-items: center;
+		margin-left: 15px;
+		height: 70px;
+		width: 100%;
+		padding: 0px 29px;
+		border: 1px solid #d6d3d1;
+		border-radius: 10px;
+	}
+
+	.row input:focus {
+		border: 3px solid #fa8960;
+		padding: 0px 27px;
+		outline: none;
 	}
 </style>
